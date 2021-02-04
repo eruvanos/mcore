3a384af7947dd02056e0afbeca2ce9170f0bbc5f
Maic Siemering <maic@siemering.tech>
2021-02-04 03:39:12 -0800

    Add datetime to json encoder

--- mcore/serialize.py
+++ mcore/serialize.py
@@ -1,9 +1,15 @@
 import dataclasses
+from datetime import datetime
 from json.encoder import JSONEncoder
 
 
 class EnhancedJSONEncoder(JSONEncoder):
+    """
+    json.dumps(datetime.today(), cls=EnhancedJSONEncoder)
+    """
     def default(self, o):
         if dataclasses.is_dataclass(o):
             return dataclasses.asdict(o)
+        elif isinstance(o, datetime):
+            return o.isoformat()
         return super().default(o)
